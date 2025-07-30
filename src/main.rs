fc78c71ce4c403f44574cb707758ab4c66d884e2
Desyatkov Sergey <sergeidesyatkov@gmail.com>
2025-07-31 00:54:13 +0500

    feat: minimal structure for CLI commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use clap::{
+    Parser,
+    Subcommand,
+};
+
+#[derive(Parser)]
+#[command(name = "raur")]
+#[command(version = "0.1.0")]
+#[command(about = "RAUR - Rusty AUR helper", long_about = "RAUR is an Arch User Repository helper for managing AUR packages with ease")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Install {
+        package: String,
+    },
+    Update {
+        package: String,
+    },
+    Remove {
+        package: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Install { package } => {
+            // TODO: Make it actually install package
+
+            println!("Installing {package}...");
+        }
+        Commands::Update { package } => {
+            // TODO: Make it actually update package
+
+            println!("Updating {package}...");
+        }
+        Commands::Remove { package } => {
+            // TODO: Make it actually remove package
+
+            println!("Removing {package}...");
+        }
+    }
 }
